fdbf2ed6502eeeff11df7fb5f79502369e1a6268
chlwlgur2257 <chlwlgur2257@naver.com>
2021-09-07 14:02:33 +0900

    나는 요리사다(배열).file

---
+++ baekjoon/2953.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sc[5][5]={0};
+    int max=0;
+    int count=0;
+
+    for(int i=0;i<5;i++)
+    {
+        int sum=0;
+        for(int j=0;j<4;j++)
+        {
+            cin>>sc[i][j];
+            sum+=sc[i][j];
+        }
+        if(sum>max)
+        {
+            max=sum;
+            count=i+1;       
+        }
+    }
+    cout<<count<<" "<<max;
+}
