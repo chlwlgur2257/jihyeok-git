9801d1a3f50440d75428e236e88d154811d1fb81
chlwlgur2257 <chlwlgur2257@naver.com>
2021-09-10 16:30:33 +0900

    소트인사이드(sort,string).file

---
+++ baekjoon/1427.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+    string n;
+    cin>>n;
+    sort(n.begin(),n.end(), greater<char>());
+    cout<<n;
+}
