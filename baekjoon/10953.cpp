b25c0c1c34f9b84bec805c8e315308bd8dc4c5be
chlwlgur2257 <chlwlgur2257@naver.com>
2021-09-13 16:50:32 +0900

    A+B-6.file

---
+++ baekjoon/10953.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int t;
+    string s;
+    int a,b;
+    char c;
+    cin>>t;
+
+    for(int i=0;i<t;i++)
+    {
+        cin>>a>>c>>b;
+        cout<<a+b<<"\n";
+    }
+}
