3c0980cdd2dd9523b3812be5bbe562e0f8504e4f
chlwlgur2257 <chlwlgur2257@naver.com>
2021-09-22 17:40:16 +0900

    x보다 작은수(배열안에 변수 사용).file

---
+++ baekjoon/10871.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N, X;
+    cin>>N>>X;
+    
+    int arr[N];
+    for(int i = 0; i < N; i++)
+    {
+        cin>>arr[i];
+    	if(arr[i] < X)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+}
