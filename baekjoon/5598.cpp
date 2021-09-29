2417a5011834b2a81e14f80aafdc932ebbf44cfb
chlwlgur2257 <chlwlgur2257@naver.com>
2021-09-29 20:10:31 +0900

    카이사르 암호.file

---
+++ baekjoon/5598.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	string str;
+	cin >> str;
+
+	for (int i = 0; i < str.size(); i++) {
+		str[i] -= 3;
+
+		if (str[i] < 'A') str[i] += 26;
+	}
+	cout << str;
+}
