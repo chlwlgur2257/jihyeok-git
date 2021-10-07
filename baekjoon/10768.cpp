472f4362fe61cb4f1dba1d3ddbfb2f5974d40064
chlwlgur2257 <chlwlgur2257@naver.com>
2021-10-07 21:07:50 +0900

    10768.file

--- baekjoon/10768.cpp
+++ baekjoon/10768.cpp
@@ -7,17 +7,21 @@ int main() {
     int a,b;
     cin>>a>>b;
  
-    if(a==2){
-        if(b==18){
+    if(a==2)
+    {
+        if(b==18)
+        {
             cout<<"Special";
         }
-        else if(b>18){
+        else if(b>18)
+        {
             cout<<"After";
         }
         else cout<<"Before";
     }
  
-    else if(a>2){
+    else if(a>2)
+    {
         cout<<"After";
     }
  
