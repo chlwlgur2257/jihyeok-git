7bc41b643c65f1a632b3e23ec803f1fb7f8db7da
chlwlgur2257 <chlwlgur2257@naver.com>
2021-11-18 14:01:44 +0900

    test.file

--- baekjoon/0000.cpp
+++ baekjoon/0000.cpp
@@ -15,8 +15,8 @@ int main() {
         for (int i = 0; i < k.size(); i++)
             a[k[i] - 'A'] = 1;
 
+
         for(int i = 0; i<26; i++)
-        
         {
             if (a[i] == 0) { ans += i + 'A'; }
         }
