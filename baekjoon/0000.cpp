1e68d0ca44a73ac476b745c6f91eb610b875a0c2
chlwlgur2257 <chlwlgur2257@naver.com>
2021-12-07 01:34:22 +0900

    test.file

--- baekjoon/0000.cpp
+++ baekjoon/0000.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
-#include <string>
-using namespace std;
-string k;
-int t, ans, a[26];
-int main() {
-    cin >> t;
-    while (t--)
+#include<stdio.h>
+int main()
+{
+    int num,sum=0;
+    printf("enter a naum ver : ");
+    scanf("%d", &sum);
+
+    for(int i=1;i<=sum; i++)
     {
-        ans = 0;
-        
-        for (int i = 0; i < 26; i++)
-            a[i] = 0;
-        cin >> k;
-        for (int i = 0; i < k.size(); i++)
-            a[k[i] - 'A'] = 1;
-        for(int i = 0; i<26; i++)
-        {
-            if (a[i] == 0) { ans += i + 'A'; }
-        }
-        cout << ans << '\n';
+
+        printf("i +");
     }
+    
+    for(int i=1;i<=sum; i++)
+    {
+        printf("sum +=i");
+    }
+    printf("Resulr : %d\n", sum);
 }
