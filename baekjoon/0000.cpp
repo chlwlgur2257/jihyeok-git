77b1b3efaed963eedcbb8d88ba8a32e83e99442f
chlwlgur2257 <chlwlgur2257@naver.com>
2021-12-16 01:43:01 +0900

    test.file

--- baekjoon/0000.cpp
+++ baekjoon/0000.cpp
@@ -5,16 +5,13 @@ int main()
     int num,sum=0;
     printf("enter a naum ver : ");
     scanf("%d", &sum);
-
     for(int i=1;i<=sum; i++)
     {
         printf("i +");
     }
-    
     for(int i=1;i<=sum; i++)
     {
         printf("sum +=i");
     }
-    
     printf("Resulr : %d\n", sum);
 }
